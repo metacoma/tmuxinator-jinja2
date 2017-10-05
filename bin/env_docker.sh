3e41ac43ee44605a06c8960c084c4d1eb5a21e2e
bebebeko <m3tac0ma@gmail.com>
2017-10-05 19:24:29 +0400

    add apocng slave

---
+++ bin/env_docker.sh
@@ -0,0 +1,1 @@
+docker run -v`pwd`:/data --rm vikingco/jinja2cli --format=yaml /data/k8s/slave_docker.yml /data/k8s/env_slave_docker.yml > slave_docker_rendered.yml && docker run -v`pwd`:/root/.tmuxinator --rm stevenaldinger/tmuxinator:latest debug slave_docker_rendered | ssh rundeck bash -
