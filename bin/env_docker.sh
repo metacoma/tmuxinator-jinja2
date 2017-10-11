94785e79970ce8fe31fd8fa1349f643f4fb1c49f
bebebeko <m3tac0ma@gmail.com>
2017-10-11 13:13:40 +0400

    update

--- bin/env_docker.sh
+++ bin/env_docker.sh
@@ -1,1 +1,4 @@
+/home/ryabin/tmp/tmuxinator-jinja2
+echo "env_id: $1" > k8s/env_slave_docker.yml
 docker run -v`pwd`:/data --rm vikingco/jinja2cli --format=yaml /data/k8s/slave_docker.yml /data/k8s/env_slave_docker.yml > slave_docker_rendered.yml && docker run -v`pwd`:/root/.tmuxinator --rm stevenaldinger/tmuxinator:latest debug slave_docker_rendered | ssh rundeck bash -
+ssh -t rundeck tmux attach -t $1
